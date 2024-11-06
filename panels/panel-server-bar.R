7e8d4e5e3584f860ef7b39f44f3a57d7dbd0c431
paulzierep <paul.zierep@googlemail.com>
2024-11-06 16:10:16 +0100

    bar

--- panels/panel-server-bar.R
+++ panels/panel-server-bar.R
@@ -65,3 +65,13 @@ output$download_bar <- downloadHandler(
             width=input$width_bar, height=input$height_bar, dpi=300L, units="in")
   }
 )
+
+observeEvent(input$store_bar, {
+      output_file <- file.path(Sys.getenv('SHINY_OUTPUT_DIR'), paste0("Bar_", simpletime(), ".", input$downtype_bar))
+
+      ggsave2(output_file,
+            plot=finalize_bar_plot(),
+            device=input$downtype_bar,
+            width=input$width_bar, height=input$height_bar, dpi=300L, units="in")
+      system(paste('put -p ', output_file))
+})
